c978b63052eb1115d4ab5af6a045a2bf07c2cbd6
Pratapdangeti <pratapdangeti@gmail.com>
2016-01-03 01:09:55 +0530

    added word 2nd chapter

--- 02.ReplacingnCorrectingWords.py
+++ 02.ReplacingnCorrectingWords.py
@@ -1,9 +1,16 @@
 __author__ = 'pratap'
 
+# Porter Stemmer
 from nltk.stem import PorterStemmer
 stemmer = PorterStemmer()
 print stemmer.stem('cooking')
 print stemmer.stem('cookery')
+
+# Lancaster Stermmer
+from nltk.stem import LancasterStemmer
+lanc_stemmer = LancasterStemmer()
+print lanc_stemmer.stem('cooking')
+print lanc_stemmer.stem('cookery')
 
 
 
