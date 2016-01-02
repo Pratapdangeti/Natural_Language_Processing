ec58b4c4e44cb1f15b24a2f857a025f7fc6d1eba
Pratapdangeti <pratapdangeti@gmail.com>
2016-01-03 00:53:37 +0530

    added word 2nd chapter

--- 01.TokenizingnWordnetBasics.py
+++ 01.TokenizingnWordnetBasics.py
@@ -15,7 +15,7 @@ import nltk.data
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 print tokenizer.tokenize(para)
 
-"""
+
 
 
 # Tokenizing sentences into words
@@ -26,6 +26,50 @@ print word_tokenize("can't")
 from nltk.tokenize import TreebankWordTokenizer
 tokenizer = TreebankWordTokenizer()
 print tokenizer.tokenize('Hello World. ')
+
+
+# Word punct tokenizer
+from nltk.tokenize import WordPunctTokenizer
+tokenizer = WordPunctTokenizer()
+print tokenizer.tokenize("Can't is a contraction.")
+
+# Tokenizing sentences using Regex
+from nltk.tokenize import RegexpTokenizer
+tokenizer = RegexpTokenizer("[\w']+")
+print tokenizer.tokenize("Can't is a contraction.")
+
+
+from nltk.tokenize import regexp_tokenize
+print regexp_tokenize("Can't is a contraction.","[\w']+")
+
+swtokenizer = RegexpTokenizer('\s+', gaps=True)
+print swtokenizer.tokenize("Can't is a contraction.")
+
+"""
+
+
+# filtering stopwords in a tokenized sentence
+from nltk.corpus import stopwords
+english_stops = set(stopwords.words('english'))
+words = ["Can't",'is','a','contraction']
+print [word for word in words if word not in english_stops]
+
+
+
+# looking up synsets for a word in wordnet
+from nltk.corpus import wordnet
+syn = wordnet.synsets('cookbook')[0]
+print syn.name
+print syn.definition
+
+print wordnet.synsets('cooking')[0].examples
+
+
+
+
+
+
+
 
 
 
---
+++ 02.ReplacingnCorrectingWords.py
@@ -0,0 +1,6 @@
+__author__ = 'pratap'
+
+from nltk.stem import PorterStemmer
+stemmer = PorterStemmer()
+print stemmer.stem('cooking')
+print stemmer.stem('cookery')
